32caf7bb391374dc48f9e40947ee81477ce14d2f
shyamal Shah <shah.shyamal.872@gmail.com>
2018-03-04 00:29:12 +0530

    changed req struct and divided QB acc type

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 # quetype = [type1,marks1,type2,marks2,....typen,marksn]
 # chapdist = [chap1,marks1,chap2,marks2....chapn,marksn]
 
-req = [1,70,[0,5,1,65],[1,5,2,10,3,10,4,5,5,5,6,10,7,5,8,5,9,10,10,5]]
+req = [1,70,[[0,5],[1,13]],[5,10,10,5,5,10,5,5,10,5]]
+typePattern =  req[2]
+chapPattern = req[3]
 queBank = []
 ftest = open("testQue.txt",'r')
 for line in ftest:
     line = line.strip()
-    que = [line[1],line[4],line[7],line[10]]
+    que = [int(line[1]),int(line[4]),int(line[7]),int(line[10])]
     queBank.append(que)
-print(queBank)
-
-# hi bhav
+# print(queBank)
+for eachtype in typePattern:
+    # print(type)
+    type = eachtype[0]
+    noOfQue = eachtype[1]
+    print("type ==>",type,"no of questions ==>",noOfQue)
+    nTypeQue = []
+    for each in queBank:
+        # print(each[2])
+        if each[2]==type:
+            nTypeQue.append(each)
+    print(nTypeQue)
+    print(len(nTypeQue))
+    nTypeQue=[]
