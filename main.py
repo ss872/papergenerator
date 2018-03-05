3b1ef6b3b59659da76df192f0bc6232762ef4c03
shyamal Shah <shah.shyamal.872@gmail.com>
2018-03-06 00:34:03 +0530

    Algo without prob & diff

--- main.py
+++ main.py
@@ -2,21 +2,38 @@
 # quetype = [type1,marks1,type2,marks2,....typen,marksn]
 # chapdist = [chap1,marks1,chap2,marks2....chapn,marksn]
 
+Result = []
+
 req = [1, 70, [[0, 5, 1], [1, 13, 5]], [5, 10, 10, 5, 5, 10, 5, 5, 10, 5]]
 typePattern = req[2]
 chapPattern = req[3]
 queBank = []
-ftest = open("testQue.txt", 'r')
-for line in ftest:
-    line = line.strip()
-    que = [int(line[1]), int(line[4]), int(line[7]), int(line[10])]
-    queBank.append(que)
-# print(queBank)
+que = []
+for dif in range(3):
+    for marks in range(1,6):
+        for type in range(2):
+            for chap in range(1,11):
+                que.append(dif)
+                que.append(marks)
+                que.append(type)
+                que.append(chap)
+                # ftest.write(str(que))
+                # ftest.write('\n')
+                queBank.append(que)
+                que = []
+# ftest = open("testQue.txt", 'r')
+# for line in ftest:
+#     line = line.strip()
+#     que = [int(line[1]), int(line[4]), int(line[7]), int(line[10])]
+#     queBank.append(que)
+
+print(queBank)
 for eachtype in typePattern:
     # print(type)
     type = eachtype[0]
     noOfQue = eachtype[1]
     weightage = eachtype[2]
+    print("Weightage ==> ",weightage)
     # print("type ==>",type,"no of questions ==>",noOfQue)
     nTypeQue = []
     for each in queBank:
@@ -28,8 +45,46 @@ for eachtype in typePattern:
     m = max(chapPattern)
     max_index = [i for i, j in enumerate(chapPattern) if j == m]  # find index of maximum weightage chapters
     print(max_index)
-    print(chapPattern)
+    # print(chapPattern)
     noOfChap = len(chapPattern)
     while noOfQue > noOfChap:
-        print("whats up")
-        noOfQue -= noOfChap
+        # print("whats up")
+        chapPattern[:] = [x - weightage for x in chapPattern]
+        # print(chapPattern)
+        for chap in range(1, 11):
+            for each in nTypeQue:
+                # print("each3  ",each[3]," chap ",chap)
+                if each[3]== chap:
+                    Result.append(each)
+                    noOfQue -= 1
+                    break #Remove break for probablistic selection
+    while noOfQue > len(max_index):
+        print("Loop ==>",noOfQue)
+        for index in max_index:
+            chapPattern[index] -= weightage
+            chap = index + 1
+            for each in nTypeQue:
+                # print("each3  ", each[3], " chap ", chap)
+                if each[3] == chap:
+                    Result.append(each)
+                    noOfQue -= 1
+                    break
+        m = max(chapPattern)
+        max_index = [i for i, j in enumerate(chapPattern) if j == m]  # find index of maximum weightage chapters
+
+    for index in max_index:
+        chapPattern[index] -= weightage
+        chap = index + 1
+        for each in nTypeQue:
+            # print("each3  ", each[3], " chap ", chap)
+            if each[3] == chap:
+                Result.append(each)
+                noOfQue -= 1
+                break
+        if noOfQue == 0:
+            break
+    print(noOfQue)
+
+
+print(Result)
+print("Final Count!! = ",len(Result))
